4ddac83e1653a2bc766d336dcce21ffa9bdadad1
AgungDH <agungdh@live.com>
2025-12-31 13:23:23 +0700

    feat: Initialize new Spring Boot project with Maven wrapper and basic Spring Boot application structure.

---
+++ src/main/java/id/my/agungdh/pajakdthrth/PajakDthrthApplication.java
@@ -0,0 +1,13 @@
+package id.my.agungdh.pajakdthrth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PajakDthrthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PajakDthrthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/agungdh/pajakdthrth/PajakDthrthApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.agungdh.pajakdthrth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PajakDthrthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
